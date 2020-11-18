04c7a49d55cafa9278403c0886622a336a975ca1
asifm <asifmohamed71656@gmail.com>
2020-11-18 21:50:39 +0530

    commited by MDAsif

---
+++ OctProject/src/webtable/Question3.java
@@ -0,0 +1,27 @@
+package webtable;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\asifm\\git\\repository\\New\\Driver\\chromedriver.exe");
+		WebDriver w = new ChromeDriver();
+		w.get("http://demo.guru99.com/test/write-xpath-table.html");
+		w.manage().window().maximize();
+		String title = w.getTitle();
+		System.out.println("Title of the page: " + title);
+		String currentUrl = w.getCurrentUrl();
+		System.out.println("CurrentUrl: " + currentUrl);
+		List<WebElement> o = w.findElements(By.tagName("tr"));
+		System.out.println("The number of rows is : "+o.size());
+	}
+
+}
