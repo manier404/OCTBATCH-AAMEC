f3df90ef786ba336c849c07e87dae5fea1388d7b
Manoj <Manoj@DESKTOP-2QRCUD1>
2020-11-18 15:41:29 +0000

    commited by manoj

--- OctProject/src/citi/webtabe/Manoj.java
+++ OctProject/src/citi/webtabe/Manoj.java
@@ -1,10 +1,28 @@
 package citi.webtabe;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Manoj {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Manoj\\eclipse-workspace\\new\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.guru99.com/test/write-xpath-table.html");
+		driver.manage().window().maximize();
+
+		List<WebElement> path = driver.findElements(By.tagName("td"));
+		for (WebElement i : path) {
+
+			String textTable = i.getText();
+			System.out.println("Table content:" + textTable);
+		}
 	}
 
 }
