189c0000318aafd736c9a7eecebc0816ee71ba29
shree <shree@shree.mshome.net>
2020-11-19 21:14:04 +0530

    commit

--- OctProject/src/citi/webtabe/WebTable2.java
+++ OctProject/src/citi/webtabe/WebTable2.java
@@ -2,6 +2,9 @@ package citi.webtabe;
 //Print all the values in first row
 import java.util.List;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 
 public class WebTable2 {
