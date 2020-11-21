712aa502618ef6f737a8eca74b0c27465d1d7b37
Manoj <Manoj@DESKTOP-2QRCUD1>
2020-11-21 17:15:28 +0000

    commited by manoj

--- OctProject/src/citi/webtabe/Manoj.java
+++ OctProject/src/citi/webtabe/Manoj.java
@@ -14,15 +14,37 @@ public class Manoj {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\Manoj\\eclipse-workspace\\new\\Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://demo.guru99.com/test/write-xpath-table.html");
-		driver.manage().window().maximize();
+		driver.get("https://www.w3schools.com/html/html_tables.aspw");
+		
+		
+		List<WebElement> tableElements = driver.findElements(By.tagName("tr"));
+	    for (int i = 10; i < tableElements.size(); i++) {
+	    	if(i%2 != 0) {
+	    	WebElement Element = tableElements.get(i);
+	    	String textOfElements = Element.getText();
+	    	System.out.println("\n"+textOfElements);
+	    
+		// driver.manage().window().maximize();
+		/* List<WebElement> tableElements = driver.findElements(By.tagName("tr"));
+		    for (int i = 10; i < tableElements.size(); i++) {
+		    	WebElement Element = tableElements.get(i);
+		    	String textOfElements = Element.getText();
+		    	System.out.println("\n"+textOfElements);
+		    */
+	/*	List<WebElement> findElements = driver.findElements(By.tagName("tr"));
+		 WebElement getElements = findElements.get(1);
+		    System.out.println("Elements in first row: "+getElements.getText());*/
+		    
+	// 3rd
+		//int size = path.size();
+   //  2nd
+		/*WebElement findElement = driver.findElement(By.xpath("//td[text()='first cell']"));
+		String text = findElement.getText();
+		System.out.println(text);
+		WebElement findElement1 = driver.findElement(By.xpath("//td[text()='third cell']"));
+		String text2 = findElement1.getText();
+		System.out.println(text2);*/
 
-		List<WebElement> path = driver.findElements(By.tagName("td"));
-		for (WebElement i : path) {
-
-			String textTable = i.getText();
-			System.out.println("Table content:" + textTable);
-		}
 	}
 
-}
+	}}}
