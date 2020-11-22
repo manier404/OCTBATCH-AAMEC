524e4c296e9a6a0a38c2287ece46d7d161a4757e
welcome <welcome@DESKTOP-JDE6VO6>
2020-11-22 12:20:58 +0530

    commited by shanthini

---
+++ OctProject/src/citi/session/Alerts.java
@@ -0,0 +1,26 @@
+package citi.session;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\welcome\\Downloads\\selenium class\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Alerts.html");
+		driver.manage().window().maximize();
+		String s1 = driver.getTitle();
+		System.out.println("Title is : " + s1);
+		String s2 = driver.getCurrentUrl();
+		System.out.println("URL is :" + s2);
+		
+		driver.findElement(By.xpath("//button[@class='btn btn-danger']")).click();
+		driver.switchTo().alert().accept();
+		
+
+	}
+
+}
