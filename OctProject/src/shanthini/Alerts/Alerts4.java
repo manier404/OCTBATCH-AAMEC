f5969aaa73e6b1638179c4b8e88e5a8d7e1be3fd
welcome <welcome@DESKTOP-JDE6VO6>
2020-11-22 12:46:34 +0530

    commited by shanthini

--- OctProject/src/shanthini/Alerts/Alerts4.java
+++ OctProject/src/shanthini/Alerts/Alerts4.java
@@ -1,9 +1,28 @@
 package shanthini.Alerts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Alerts4 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\welcome\\Downloads\\selenium class\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://netbanking.hdfcbank.com/netbanking/?_ga=2.176378149.1819882415.1533883212-608727520.1532670997");
+		driver.manage().window().maximize();
+		String s1 = driver.getTitle();
+		System.out.println("Title is : " + s1);
+		String s2 = driver.getCurrentUrl();
+		System.out.println("URL is :" + s2);
+		
+		driver.switchTo().frame(0);
+		
+		driver.findElement(By.xpath("//img[@alt='continue']")).click();
+		
+		Thread.sleep(1000);
+		driver.switchTo().alert().accept();
 
 	}
 
